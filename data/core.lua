0d57406d04d1199f46d2cd45972eb257f8114791
Keijin <keijin.der.suchti@gmail.com>
2025-08-26 19:14:10 +0200

    v0.9.0

--- data/core.lua
+++ data/core.lua
@@ -2,7 +2,7 @@
 -- Entry point and setup logic for KeijinAchievementMonitorNEW
 
 -- Version (zentral definiert)
-KAMN_VERSION = "0.8.1"
+KAMN_VERSION = "0.9.0"
 
 -- Check for global corruption (classic safeguard)
 if type(string) ~= "table" then
